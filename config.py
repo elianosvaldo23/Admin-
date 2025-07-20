cfe75823249153ecdc7b4868f3d30b38e8fa6496
elianosvaldo23 <elianverdeciamesa@gmail.com>
2025-07-20 07:09:03 -0400

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-
-MONGO_URI = "MONGODB_URI", "mongodb+srv://zoobot:zoobot@zoolbot.6avd6qf.mongodb.net/zoolbot?retryWrites=true&w=majority&appName=Zoolbot"  # Replace with your actual MongoDB connection string
-DEFAULT_WELCOME_MESSAGE = "Welcome to the bot!"  # Replace with your desired default message
